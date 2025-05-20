23c65ded941a04777dfb955f76688acd49b50ec4
Mbayang <diedhioumbayang1321@gmail.com>
2025-05-20 16:20:57 +0000

    build du microservice userService et visualisation dans jhipster registry OK

---
+++ userService/src/test/java/com/universite/IntegrationTest.java
@@ -0,0 +1,23 @@
+package com.universite;
+
+import com.universite.config.AsyncSyncConfiguration;
+import com.universite.config.EmbeddedSQL;
+import com.universite.config.JacksonConfiguration;
+import com.universite.config.TestSecurityConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = { UserServiceApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class }
+)
+@EmbeddedSQL
+public @interface IntegrationTest {
+}
